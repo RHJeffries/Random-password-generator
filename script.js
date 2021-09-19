73121f59e3990222c1d8b45782b4028153aca31b
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-09-19 22:45:22 +0930

    minor changes to js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Criteria variables for password
 
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+var lower = ["abcdefghijklmnopqrstuvwxyz"]
+var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var numbers = ["0123456789"]
+var special = ["!@#$%^&*()+-.`~|<>=-_"]
 
 // Function to begin crtieria selection
 
@@ -17,24 +17,22 @@ var generatePassword = function() {
         return ;
     }
     
-    
     var answerLower = window.confirm("Do you want lower case letters included? OK - yes, Cancel - no");
-    if (answerLower == true);
-    
+     
     var answerUpper = window.confirm("Do you want upper case letters included? OK - yes, Cancel - no");
-    if (answerUpper == true);
 
     var answerNumbers = window.confirm("Do you want numbers included? OK - yes, Cancel - no");
-    if (answerNumbers == true);
 
     var answerSpecial = window.confirm("Do you want special characters included? OK - yes, Cancel - no");
-    if (answerSpecial == true);
+      if 
+        ((answerLower == true) || (answerUpper == true) || (answerNumbers == true) || (answerSpecial == true));
+      
 
     else {
-        ((lower == false) && (upper == false) && (numbers == false) && (special == false))
+        ((answerLower == false) && (answerUpper == false) && (answerNumbers == false) && (answerSpecial == false))
     window.alert("Must choose one criteria of lower case, upper case, numbers or special characters");
     }
-    return;
+    return ;
 }
 
 
@@ -43,7 +41,7 @@ var generatePassword = function() {
 
 // Function to create password
 
-function createPword() {
+function password() {
 
   var pwordOptions = criteria;
   var pwordCombination = [];
@@ -59,7 +57,7 @@ function createPword() {
     
   }
 
-  return finalPassword;
+  return pwordResult;
 
 }
 
