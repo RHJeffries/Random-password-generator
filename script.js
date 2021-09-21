6df422f384501d615526a244fb46d3d8cd70a44f
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-09-22 00:14:24 +0930

    script updated again, removed return availableChars; and it now works. Finally

--- script.js
+++ script.js
@@ -8,17 +8,22 @@ var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
 
-// Function to begin crtieria selection
+// Function to generate password
 function generatePassword() {
+
+// Password variables, to be filled by selections
   var availableChars = [];
+  var pwordResult = "";
+
+// Variable to determine password length, validate selection between 8 and 128, and is a number entered
   var pwordLength = window.prompt("Select number of character between 8 and 128");
   console.log(pwordLength);
   if ((pwordLength < 8) || (pwordLength > 128) || isNaN(pwordLength)) {
     window.alert("Must be between 8 and 128 characters");
-    return;
+    return pwordLength;
   }
 
-
+// Criteria to determine make up of password characters
   var answerLower = window.confirm("Do you want lower case letters included? OK - yes, Cancel - no");
   console.log(answerLower);
   var answerUpper = window.confirm("Do you want upper case letters included? OK - yes, Cancel - no");
@@ -33,37 +38,26 @@ function generatePassword() {
     }
     if (answerUpper) {
       availableChars = availableChars.concat(upper)
-    } 
+    }
     if (answerNumbers) {
       availableChars = availableChars.concat(numbers)
-    }  
+    }
     if (answerSpecial) {
       availableChars = availableChars.concat(special)
-    } 
-     
+    }
+
   }
   else {
     window.alert("Must choose one criteria of lower case, upper case, numbers or special characters");
-  } {
-  return; }
-
-  for (var i = 0; i < pwordOptions.length; i++) {
-    pwordResult += availableChars[Math.floor(Math.random() * pwordCombo.length)];
-
   }
 
+// For statement to produce a random string from the password length and criteria
+  for (var i = 0; i < pwordLength; i++) {
+    pwordResult += availableChars[Math.floor(Math.random() * availableChars.length)];
+  }
+  return pwordResult;
 
 }
-
-
-
-
-
-
-
-
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -72,6 +66,7 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
